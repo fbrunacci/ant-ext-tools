67c02fee2ba037f3dceea45e1c7d783f499b0f4f
fbrunacci <fbrunacci@gmail.com>
2021-03-13 17:33:30 +0100

    init

---
+++ src/main/java/com/sainet/Main.java
@@ -0,0 +1,18 @@
+package com.sainet;
+
+
+import org.apache.log4j.Logger;
+
+import java.util.Arrays;
+
+// https://www.jetbrains.com/help/ruby/configuring-keyboard-and-mouse-shortcuts.html
+public class Main {
+
+//    private static final Logger logger = Logger.getLogger(Main.class);
+
+    public static void main(String[] args) {
+	    System.out.println("args:" + Arrays.asList(args));
+//	    logger.info("args:" + Arrays.asList(args));
+    }
+
+}
